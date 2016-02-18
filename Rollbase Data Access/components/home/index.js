e4f88aa9349e5d18ec51905f0335694d85d741b6
Renato Teixeira <rteixeir@progress.com>
2016-02-18 15:49:10 +0000

    Changes made by Renato Teixeira (rteixeir@progress.com).

--- Rollbase Data Access/components/home/index.js
+++ Rollbase Data Access/components/home/index.js
@@ -9,3 +9,88 @@ app.home = kendo.observable({
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
 // END_CUSTOM_CODE_home
+(function(parent) {
+    var dataProvider = app.data.progressDataProvider,
+        jsdoOptions = {
+            name: 'Carro',
+            autoFill: false
+        },
+        dataSourceOptions = {
+            type: 'jsdo',
+            transport: {},
+
+            error: function(e) {
+                if (e.xhr) {
+                    alert(JSON.stringify(e.xhr));
+                }
+            },
+            schema: {
+                model: {
+                    fields: {
+                        'name': {
+                            field: 'name',
+                            defaultValue: ''
+                        },
+                    }
+                }
+            },
+        },
+        dataSource = new kendo.data.DataSource({
+            pageSize: 50
+        }),
+        homeModel = kendo.observable({
+            dataSource: dataSource,
+            _dataSourceOptions: dataSourceOptions,
+            _jsdoOptions: jsdoOptions,
+            itemClick: function(e) {
+                app.mobileApp.navigate('#components/home/details.html?uid=' + e.dataItem.uid);
+            },
+            detailsShow: function(e) {
+                var item = e.view.params.uid,
+                    dataSource = homeModel.get('dataSource'),
+                    itemModel = dataSource.getByUid(item);
+
+                if (!itemModel.name) {
+                    itemModel.name = String.fromCharCode(160);
+                }
+
+                homeModel.set('currentItem', null);
+                homeModel.set('currentItem', itemModel);
+            },
+            currentItem: null
+        });
+
+    if (typeof dataProvider.sbProviderReady === 'function') {
+        dataProvider.sbProviderReady(function dl_sbProviderReady() {
+            parent.set('homeModel', homeModel);
+        });
+    } else {
+        parent.set('homeModel', homeModel);
+    }
+    parent.set('onShow', function() {
+        dataProvider.loadCatalogs().then(function _catalogsLoaded() {
+            var jsdoOptions = homeModel.get('_jsdoOptions'),
+                jsdo = new progress.data.JSDO(jsdoOptions),
+                dataSourceOptions = homeModel.get('_dataSourceOptions'),
+                dataSource;
+
+            dataSourceOptions.transport.jsdo = jsdo;
+            dataSource = new kendo.data.DataSource(dataSourceOptions);
+            homeModel.set('dataSource', dataSource);
+        });
+    });
+})(app.home);
+
+// START_CUSTOM_CODE_homeModel
+// you can handle the beforeFill / afterFill events here. For example:
+/*
+app.home.homeModel.get('_jsdoOptions').events = {
+    'beforeFill' : [ {
+        scope : app.home.homeModel,
+        fn : function (jsdo, success, request) {
+            // beforeFill event handler statements ...
+        }
+    } ]
+};
+*/
+// END_CUSTOM_CODE_homeModel
